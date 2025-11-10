8c99cc2e98fd77cc2e5fc044e29f3f6e9614899f
AcrossBay <massi@across-bay.com>
2025-11-10 07:32:21 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
-// tailwind.config.js
-const defaultTheme = require('tailwindcss/defaultTheme');
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        // Tailwind userà il font della variabile CSS che gli passiamo da next/font
-        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
+  content: ["./app/**/*.{js,jsx,ts,tsx,mdx}"],
+  theme: { extend: {} },
   plugins: [],
 };

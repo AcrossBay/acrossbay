ea736e95f1fefdf0d8fdcc837dea9129515fe998
AcrossBay <massi@across-bay.com>
2026-01-26 19:26:35 +0000

    Update page.js

--- app/en/page.js
+++ app/en/page.js
@@ -1,23 +1,3 @@
 export default function Page() {
-  return (
-    <main style={{ padding: "40px", textAlign: "center" }}>
-      <h1>AcrossBay</h1>
-      <p>International marketplace for selected products.</p>
-
-      <a
-        href="https://buy.stripe.com/bJe3cxd9M8VIdaa90v2kw00"
-        style={{
-          display: "inline-block",
-          marginTop: "24px",
-          padding: "12px 24px",
-          backgroundColor: "#000",
-          color: "#fff",
-          textDecoration: "none",
-          borderRadius: "6px",
-        }}
-      >
-        Pay now
-      </a>
-    </main>
-  );
+  return null;
 }

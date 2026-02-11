f428281c263a9b0879ea7749ea93f1a1c97235af
AcrossBay <massi@across-bay.com>
2026-02-11 11:26:54 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -18,8 +18,34 @@ export default function RootLayout({ children }) {
           width: "100%",
           maxWidth: "100vw",
           overflowX: "hidden",
+          fontFamily: "Arial, sans-serif",
         }}
       >
+        {/* Header */}
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "flex-end",
+            padding: "20px",
+            boxSizing: "border-box",
+          }}
+        >
+          <a
+            href="https://acrossbay-b2b-portal.vercel.app/login"
+            style={{
+              backgroundColor: "black",
+              color: "white",
+              padding: "10px 20px",
+              textDecoration: "none",
+              borderRadius: "4px",
+              fontSize: "14px",
+            }}
+          >
+            Area B2B
+          </a>
+        </div>
+
         {children}
       </body>
     </html>
